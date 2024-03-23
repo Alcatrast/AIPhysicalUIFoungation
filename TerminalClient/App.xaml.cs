d666acc2410ccf16ff7d2675d639ea7aa152b687
ALCATRAST_LEFT <leonid.efremov.99@gmail.com>
2024-03-23 15:41:50 +0300

    Добавьте файлы проекта.

---
+++ TerminalClient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TerminalClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TerminalClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TerminalClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
